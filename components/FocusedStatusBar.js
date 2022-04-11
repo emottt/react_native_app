1c77376b317427ae654c662421affdfb0aa0331b
emottt <31847517+emottt@users.noreply.github.com>
2022-04-11 18:45:55 +0300

    initial commit

---
+++ components/FocusedStatusBar.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import { useIsFocused } from '@react-navigation/native';
+import { StatusBar } from 'react-native-web';
+
+const FocusedStatusBar = (props) => {
+  const isFocused = useIsFocused();
+  return isFocused ? <StatusBar animated={ true } { ...props } /> : null;
+  
+}
+
+export default FocusedStatusBar
---
+++ components/index.js
@@ -0,0 +1,12 @@
+import FocusedStatusBar from './FocusedStatusBar';
+import HomeHeader from './HomeHeader';
+import NFTCard from './NFTCard';
+import { CircleButton, RectButton } from './Button';
+import { SubInfo } from './SubInfo';
+import DetailsDesc from './DetailsDesc';
+import DetailsBid from './DetailsBid';
+
+
+export {
+    FocusedStatusBar, HomeHeader, NFTCard, CircleButton, RectButton, DetailsDesc, DetailsBid, SubInfo,
+}
